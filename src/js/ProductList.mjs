08700f187ab70ba5512618c1678139b973cbe07a
dustyjem <97073289+dustyjem@users.noreply.github.com>
2023-02-03 12:31:50 -0700

    product list

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -23,6 +23,7 @@ export default class ProductList {
       // our dataSource will return a Promise...so we can use await to resolve it.
       const list = await this.dataSource.getData();
       // render the list 
+      renderList()
     }
     
   }
