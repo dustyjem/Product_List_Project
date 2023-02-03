79946d6c9fb2240c3691a54032ee5c5ba8a4eece
dustyjem <97073289+dustyjem@users.noreply.github.com>
2023-02-03 11:26:31 -0700

    lint

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
-import ProductData from './ProductData.mjs';
-import ProductDetails from './ProductDetails.mjs';
-import { getParam } from './utils.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { getParam } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 
@@ -9,20 +9,20 @@ const productId = getParam("Id");
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product)
-  const myProduct = getLocalStorage("so-cart")
-  //Used this section to trick lint
-  var test_er = myProduct
-  test_er
-}
+// function addProductToCart(product) {
+//   setLocalStorage("so-cart", product)
+//   const myProduct = getLocalStorage("so-cart")
+//   //Used this section to trick lint
+//   var test_er = myProduct
+//   test_er
+// }
 
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
 
-}
+// }
 
 // add listener to Add to Cart button
 
