97795a4e238055c9b17cabe78e890d096554b728
dustyjem <97073289+dustyjem@users.noreply.github.com>
2023-02-18 12:40:59 -0700

    Alot of updates. We are caught up!

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import ProductData from "./ProductData.mjs"
 import ProductList from "./ProductList.mjs"
 
-const ProductData = new ProductData("tents")
+const dataSource = new ProductData("tents")
+const ProductListElement = document.querySelector(".product-list")
 
-const ProductList = new ProductList("carts")
+const ProductList = new ProductList("tents", dataSource, ProductListElement)
+
+ProductList.init()
 
