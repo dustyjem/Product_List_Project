4bc212abc77b4b8bd01a616307cc168f8fdd014e
dustyjem <97073289+dustyjem@users.noreply.github.com>
2023-02-20 15:01:45 -0700

    Search menu and checkout - Not responsive

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,11 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
 
 // Render Items for the first load
 const shoppingCart = new ShoppingCart(`.product-list`, `so-cart`);
 loadHeaderFooter("../partials/");
 shoppingCart.init();
+if (cart.total > 0) {
+    // show our checkout button and total if there are items in the cart.
+    document.querySelector(".list-footer").classList.remove("hide");
+  }
